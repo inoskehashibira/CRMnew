eca5a501c13c2f756136ce4ef7eed5be749b16b2
Inoske hashibira <shakhawat.iiuc.se@gmail.com>
2023-01-03 11:24:09 +0600

    card is working properly

--- Frontend/components/card.php
+++ Frontend/components/card.php
@@ -9,25 +9,83 @@
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css” />
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css” />
+
 
     <title>Template</title>
 </head>
 
+<!-- <i class="bi bi-three-dots-vertical"></i> -->
+
+
 <body>
 
-    <div class="border border-warning my-3 px-4 py-2">
-        <i class="bi bi-three-dots-vertical"></i>
-        <h6>Name: Mr Nafis Sadik</h6>
-        <p>Bike Mode: Yamaha Fazer</p>
-        <p>Phone: 01851035422</p>
+
+
+    <div class="shadow p-1 mb-5 bg-white rounded" style="width: 10 rem;">
+        <div class="card-body">
+
+            <div class="row">
+                <div class="col-9">
+                    <h5 class="card-title">Mr ABC</h5>
+                </div>
+                <div class="col-3">
+                    <i class="bi bi-three-dots-vertical col m-0 p-0"></i>
+                </div>
+            </div>
+
+            <h6 class="card-subtitle mb-2 text-muted">Sb-1400</h6>
+            <p class="card-text">
+                Phone: 01851035422
+            </p>
+            <a href="#" class="card-link">Lead Details</a>
+        </div>
     </div>
-    <div class="border border-warning my-3 px-4 py-2">
-        <h6>Name: Mr ABC</h6>
-        <p>Car Mode: Sb-1400</p>
-        <p>Phone: 01851035422</p>
+
+
+    <div class="shadow p-1 mb-5 bg-white rounded" style="width: 10 rem;">
+        <div class="card-body">
+
+            <div class="row">
+                <div class="col-9">
+                    <h5 class="card-title">Mr ABC</h5>
+                </div>
+                <div class="col-3">
+
+                    <div class="dropdown">
+                        <a id="dLabel" data-target="#" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="bi bi-three-dots-vertical col m-0 p-0"></i>
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dLabel">
+                            <li><a class="dropdown-item" href="#">Qualified</a></li>
+                            <li><a class="dropdown-item" href="#">intermediate</a></li>
+                            <li><a class="dropdown-item" href="#">won</a></li>
+                        </ul>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+            <h6 class="card-subtitle mb-2 text-muted">Sb-1400</h6>
+            <p class="card-text">
+                Phone: 01851035422
+            </p>
+            <a href="#" class="card-link">Lead Details</a>
+        </div>
     </div>
+
+
+
     <div class="border border-warning my-3 px-4 py-2">
         <h6>Name: Mr ABC</h6>
         <p>Car Mode: Sb-1400</p>
