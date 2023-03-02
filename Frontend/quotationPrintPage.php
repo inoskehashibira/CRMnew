e8ac3040a2f527861d9f009ae10497776d06571d
Inoske hashibira <shakhawat.iiuc.se@gmail.com>
2023-03-02 15:59:38 +0600

    added the quotation printing page and completed the design by 50%

---
+++ Frontend/quotationPrintPage.php
@@ -0,0 +1,322 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="Css/quotationPrintPage.css">
+
+    <title>
+        Quotation Doc
+
+    </title>
+
+
+</head>
+
+<body>
+    <p class="gap"><br /></p>
+    <p class="gap"><br /></p>
+
+
+    <div class="container1">
+        <h2>Ref: REL/QUT/2021/01001 </h2>
+        <div class="spacer"></div>
+        <h2>Date: 23rd January, 2022</h2>
+    </div>
+
+    <div class="container2">
+        <p style=" border: 1px solid black;"> To</p>
+        <p class="boldText">NUOVO Renewable Energy Limited,</p>
+        <p> Pedrollo Plaza, 5 Jubilee Road,</p>
+        <p> Chattogram - 4000.</p>
+        <p><span class="boldText">Attn : Mr Nader Khan,</span><br>
+            <span style="margin-left: 3rem;"> Honorable Chairman.</span>
+        </p>
+        <p><span class="boldText">Phone :</span> 031-621531-5</p>
+        <p><span class="boldText">Email :</span> naderkhan@pedrollo.com.bd</p>
+
+        <p style="margin-top: 60px;">
+            Subject :<span class="boldText"><u>Price Quotation for Japan Origin Subaru XV 1.6-i</u></span>
+        </p>
+        <p class="verticalGap">Dear Sir,</p>
+        <p>
+            We express our gratitude for showing your interest in our 2019 Brand New
+            Subaru Vehicles. As you are well aware that Subaru Corporation is a car
+            manufacturing division of a huge Japanese transportation conglomerate
+            called Fuji Heavy Industries.
+        </p>
+        <p style="margin-top: 20px;">
+            As sole Agent/distributor of Subaru in Bangladesh, REL Motors Limited is
+            pleased to offer you the price quotation of the following vehicle for your
+            kind consideration.
+        </p>
+
+
+    </div>
+
+
+    <table cellspacing="0" style="margin-top: 20px;">
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Model</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">Subaru XV 1.6i (Basic)</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Manufacturer</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">Subaru Corporation</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Engine Specification</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">1.6L, 4-cylinders BOXER Technology, DOHC 16-Valve, Petrol</p>
+                <p class="colText">Engine, Engine Immobiliser.</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Year of Manufacture</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">2019, Brand New</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Country of Origin</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">Japan</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Country of Manufacture</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">Japan</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Maximum Output</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">114 PS (84 kW) or 112 HP / 6200 rpm</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Maximum Torque</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">150 Nm (15.3 kgfm) /3600 rpm</p>
+            </td>
+        </tr>
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">Displacement</p>
+            </td>
+            <td class="table1RowCol2">
+                <p class="colText">1600 cc</p>
+            </td>
+        </tr>
+    </table>
+
+
+
+
+
+
+
+
+
+
+    <table cellspacing="0">
+        <tr class="tableRow">
+            <td class="table1RowCol1">
+                <p class="colText">
+                    Standard Features
+                </p>
+            </td>
+            <td class="table1RowCol2">
+                <ul id="l1">
+                    <li>
+                        <p class="s5">
+                            Automatic Air-Conditioning System with Anti-dust Filter
+                        </p>
+                    </li>
+                    <li>
+                        <p class="s5">
+                            Fabric Seat Package with Seat Safety belts
+                        </p>
+                    </li>
+                    <li>
+                        <p class="s5">
+                            Halogen Head Lamp
+                        </p>
+                    </li>
+                    <li>
+                        <p class="s5">
+                            All Power windows
+                        </p>
+                    </li>
+                    <li data-list-text="">
+                        <p class="s5">
+                            Advanced Safety Package – Global Platform
+                        </p>
+                    </li>
+                    <li>
+                        <p class="s5">
+                            USB and Auxiliary Audio input Jacks and Bluetooth system
+                        </p>
+                    </li>
+                    <li>
+                        <p class="s5">
+                            Aluminum Alloy Wheel – Tire size: 225/60 R17, 17x7” J
+                        </p>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td class="td1">
+                <p class="s5" style="
+              padding-top: 8pt;
+              padding-left: 6pt;
+              text-indent: 0pt;
+              text-align: left;
+            ">
+                    Price/Unit
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s4">
+                    BDT 36,00,000.00
+                    <span class="s5">(BDT Thirty Six Lac ) Only, Unit Price (without BRTA Registration
+                        Fee &amp; TDS).</span>
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 81pt">
+            <td class="td1">
+                <p class="gap"><br /></p>
+                <p class="s5" style="padding-left: 6pt; text-indent: 0pt; text-align: left">
+                    Payment Terms
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5">
+                    Payment to be made to “REL Motors Limited” by A/C Payee Cheque or
+                    Pay Order. The minimum amount for booking is One Lac taka. BDT Ten
+                    Thousand will be deducted on cancellation of the order within
+                    Fifteen days. BDT One Lac will be deducted from the booking amount
+                    if the order does not close within one
+                </p>
+                <p class="s5" class="headings">
+                    month.
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 26pt">
+            <td class="td1">
+                <p class="s5">
+                    Delivery Terms
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5">
+                    Delivery within 3 days from receiving Work Order and 100%
+                </p>
+                <p class="s5">
+                    payment. Delivery place – Dhaka or Chattogram only
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 14pt">
+            <td class="td1">
+                <p class="s5">
+                    After Sales Service
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5" class="headings">
+                    3 free services after every 6,000 km +/-
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 14pt">
+            <td class="td1">
+                <p class="s5">
+                    Spare Parts
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5" class="headings">
+                    All spare parts will be provided from stock as &amp; when required
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 14pt">
+            <td class="td1">
+                <p class="s5">
+                    Offer Validity
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5" class="headings">
+                    15 days from the date of the quotation
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 81pt">
+            <td class="td1">
+                <p class="gap"><br /></p>
+                <p class="s5" style="padding-left: 6pt; text-indent: 0pt; text-align: left">
+                    Warranty
+                </p>
+            </td>
+            <td class="td2">
+                <p class="s5">
+                    1 year or 20,000 km whichever comes first, under the terms and
+                    conditions defined by the Manufacturer.
+                    <b>CNG/LPG Conversion or any other form of modification to the
+                        vehicle without prior written consent from “REL Motors Limited”
+                        will void the warranty. </b>If any parts are used in the car without the
+                </p>
+                <p class="s5">
+                    consent of REL Motors then all warranty will be void.
+                </p>
+            </td>
+        </tr>
+    </table>
+
+
+
+
+    <p class="gap"><br /></p>
+    <p>
+        We expect you will find our offer most competitive and hope to have your
+        valued order in our favor.
+    </p>
+    <p class="gap"><br /></p>
+    <p class="headings">
+        NB: For more information, please contact the undersigned.
+    </p>
+    <p class="gap"><br /></p>
+    <p>
+        AYM Fazlul Hoque <b>Head of Finance </b>REL Motors Limited
+    </p>
+    <p class="gap"><br /></p>
+    <p></p>
+    <p >
+        2 <b>| </b><span style="color: #808080">Page</span>
+    </p>
+</body>
+
+</html>
