f16d190af0f741eb301cd3eecad003af29fe23af
Inoske hashibira <shakhawat.iiuc.se@gmail.com>
2023-03-06 14:11:32 +0600

    created the invoice print page

---
+++ Frontend/invoicePage.php
@@ -0,0 +1,222 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="Css/InvoicePage.css">
+
+    <title>
+        Invoice
+
+    </title>
+
+
+</head>
+
+<body>
+
+
+    <div class="container1">
+        <table class="tableCell" style="margin-top: 20px;" cellspacing="0">
+            <tr>
+                <td class="tableCell">
+                    <p class="">
+                        Quotation Ref:
+                    </p>
+                </td>
+                <td class="tableCell">
+                    <p>
+                        13010088
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td class="tableCell">
+                    <p class="">
+                        Sales Ref:
+                    </p>
+                </td>
+                <td class="tableCell">
+                    <p class="">
+                        13040088
+                    </p>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="containerHeader">
+        <h1>COMMERCIAL INVOICE</h1>
+    </div>
+    <div class="containerHeaderElements">
+        <p style="font-size: 12pt;">Office Copy </p>
+        <p style="margin-left: 100px;font-size: 12pt;">Date : 01/02/2022</p>
+
+    </div>
+
+
+    <div class="container2">
+        <p> To</p>
+        <p class="boldText">NUOVO Renewable Energy Limited,</p>
+        <p> Pedrollo Plaza(4<sup style="margin: 0; font-size:5pt">th</sup> Floor)</p>
+        <p>5, Jubilee Road,</p>
+        <p> Chattogram sadar, Chattogram - 4000.</p>
+        <p> Bangladesh</p>
+
+    </div>
+
+    <div class="containerHeader">
+        <table class="tableCell" cellspacing="0">
+            <tr style="width: auto;">
+
+                <td class="tableCell" style="">
+                    <p class="boldText">
+                        SL NO.
+                    </p>
+                </td>
+                <td class="tableCell" style="width: 25rem;text-align: center;">
+                    <p class="boldText">
+                        DESCRIPTION
+                    </p>
+                </td>
+                <td class="tableCell" style="width: 10rem;text-align: center;">
+                    <p class="boldText">
+                        AMOUNT(BDT)
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td class="tableCell">
+                    <p class="">
+                        01
+                    </p>
+                </td>
+                <td class="tableCell">
+                    <p class="boldText">
+                        SUBARU XV Fully Loaded 1.6 i-Basic (Brand New)
+                    </p>
+                    <table class="" style="margin:0" cellspacing="0">
+                        <tr>
+                            <td class="">
+                                <p class="boldText">VIN Code</p>
+                            </td>
+                            <td class="">
+                                <p>: JFI GT3KC5KG075953</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Engine</p>
+                            </td>
+                            <td class="">
+                                <p>: 6688</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Cubic Capacity</p>
+                            </td>
+                            <td class="">
+                                <p>: 1599 (Gasoline)</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Origin</p>
+                            </td>
+                            <td class="">
+                                <p>: JAPAN</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Color</p>
+                            </td>
+                            <td class="">
+                                <p>: Crystal White Pearl</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Manufacturing Year </p>
+                            </td>
+                            <td class="">
+                                <p>: 2019</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="">
+                                <p class="boldText">Seats</p>
+                            </td>
+                            <td class="">
+                                <p>: 05 (Five) including driverd</p>
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+                <td class="tableCell" style="text-align: center;">
+                    <p class="boldText">
+                        3,600,000
+                    </p>
+                </td>
+            </tr>
+            <tr style="width: auto;">
+
+                <td class="tableCell" style="">
+
+                </td>
+                <td class="tableCell" style="width: 25rem;text-align: center;">
+                    <p class="boldText">
+                        TOTAL TAKA
+                    </p>
+                </td>
+                <td class="tableCell" style="width: 10rem;text-align: center;">
+                    <p class="boldText">
+                        3,600,000
+                    </p>
+                </td>
+            </tr>
+            <tr style="width: auto;">
+
+                <td colspan="3" class="tableCell" style="">
+
+                    <p class="boldText">
+                        In Words: Thirty Six Lac Only
+                    </p>
+                </td>
+
+            </tr>
+        </table>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container3">
+        <p style="margin-top: 20px;">
+            The Cheque/ PO/EFT will be issued in favor of <span class="boldText">“REL Motors Limited”</span>
+        </p>
+
+
+
+
+
+    </div>
+
+    <div class="container5" >
+        <p>Seller's Signature</p>
+        <p>Buyers's Signature</p>
+    </div>
+
+</body>
+
+</html>
